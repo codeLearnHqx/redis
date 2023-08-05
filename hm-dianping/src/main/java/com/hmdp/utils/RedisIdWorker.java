0a09371a393015f5cb1effee565e94588252aef4
hqx <751156604@qq.com>
2023-08-06 07:52:11 +0800

    全局唯一id生成

---
+++ hm-dianping/src/main/java/com/hmdp/utils/RedisIdWorker.java
@@ -0,0 +1,52 @@
+package com.hmdp.utils;
+
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * Redis实现全局唯一Id
+ * @Description 基于redis的id生成器
+ * @Create by hqx
+ * @Date 2023/8/6 0:55
+ */
+@Component
+public class RedisIdWorker {
+    /**
+     * 开始时间戳
+     */
+    private static final long BEGIN_TIMESTAMP = 1691284026L;
+    /**
+     * 移动的比特位数
+     */
+    private static final int COUNT_BITS = 32;
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    public long nextId(String keyPrefix) {
+        // 1. 生成时间戳
+        LocalDateTime now = LocalDateTime.now();
+        long nowSecond = now.toEpochSecond(ZoneOffset.UTC);
+        long timestamp = nowSecond - BEGIN_TIMESTAMP;
+
+        // 2. 生成序列号
+        // 2.1 获取当前日期，精确到天
+        String date = now.format(DateTimeFormatter.ofPattern("yyyy:MM:dd"));
+        // 2.2 自增长 （如果传入的key不存在会自动创建）
+        long count = stringRedisTemplate.opsForValue().increment("icr:" + keyPrefix + ":" + date);
+        // 3. 拼接返回
+        return timestamp << COUNT_BITS | count;
+    }
+
+    public static void main(String[] args) {
+        LocalDateTime time = LocalDateTime.now();
+        long epochSecond = time.toEpochSecond(ZoneOffset.UTC);
+        System.out.println(epochSecond);
+    }
+
+}
--- hm-dianping/src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ hm-dianping/src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -3,11 +3,15 @@ package com.hmdp;
 import com.hmdp.entity.Shop;
 import com.hmdp.service.impl.ShopServiceImpl;
 import com.hmdp.utils.CacheClient;
+import com.hmdp.utils.RedisIdWorker;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
@@ -15,15 +19,36 @@ import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
 @SpringBootTest
 class HmDianPingApplicationTests {
 
-    @Resource
-    private ShopServiceImpl service;
-    @Resource
-    private CacheClient cacheClient;
 
+    @Resource
+    private RedisIdWorker redisIdWorker;
+
+
+    private ExecutorService es = Executors.newFixedThreadPool(500);
     @Test
-    void testSaveShop() throws InterruptedException {
-        Shop shop = service.getById(1L);
-        cacheClient.setWithLogicalExpire(CACHE_SHOP_KEY + 1L, shop, 10L, TimeUnit.SECONDS);
+    void testIdWorker() throws InterruptedException {
+        // 300  表示在线程通过 await 之前必须调用 countDown 的次数
+        CountDownLatch latch = new CountDownLatch(300);
+
+        // 单个线程创建100id
+        Runnable task = () -> {
+            for (int i = 0; i < 100; i++) {
+                long id = redisIdWorker.nextId("order");
+                System.out.println("id = " + id);
+            }
+            latch.countDown();
+        };
+        long begin = System.currentTimeMillis();
+        // 将线程任务提交300次
+        for (int i = 0; i < 300; i++) {
+            es.submit(task);
+        }
+        latch.await();
+        long end = System.currentTimeMillis();
+        // 耗时
+        System.out.println("time = " + (end - begin));
+
+
     }
 
 }
